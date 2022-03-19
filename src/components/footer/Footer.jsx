805a259535b6cffd00e33122d2149183b951e939
MAYANK KUMAR THAKUR <mayank.sanyank@gmail.com>
2022-03-19 10:45:32 +0530

    added Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import './Footer.css'
+import { FaFacebook } from 'react-icons/fa'
+import { AiOutlineInstagram,AiOutlineGithub } from 'react-icons/ai'
+import {GrLinkedin} from 'react-icons/gr'
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer id='footer'>
+      <div className="container container__footer">
+         <div>
+         <a href="#" className='footer__logo'>Mayank</a>
+       </div>
+      <ul className='permanlink'>
+        <h3>Quick Links</h3>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact me</a></li>
+      </ul>
+
+      <div className="footer__social">
+        <a href=""><FaFacebook/></a>
+        <a href=""><AiOutlineInstagram/></a>
+        <a href=""><GrLinkedin/></a>
+        <a href=""><AiOutlineGithub/></a>
+        
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mayank Thakur. All rights reserved.</small>
+      </div>
+     </div>
+    </footer>
   )
 }
 
