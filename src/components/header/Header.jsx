dc92ef33dcd1659368591646103aebed50370083
MAYANK KUMAR THAKUR <mayank.sanyank@gmail.com>
2022-03-19 11:08:31 +0530

    changed logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Mayank Thakur</h1>
+        <h1 className='main__logo'>Mayank Thakur</h1>
         <h5 className="text-light">
           I'm a Full Stack Developer, currently working at <a href="https://www.linkedin.com/in/mayank-thakur-b9a8b9a6/">Linkedin</a>
         </h5>
