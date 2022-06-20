c57cd68ce4c5d39c990254699714ccf4689818f2
MAYANK KUMAR THAKUR <mayank.sanyank@gmail.com>
2022-06-20 09:01:39 +0530

    Updated Packages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 ReactDOM.render(
-    <>
-        <App />
-    </>
-    ,
-    document.getElementById("root")
-    
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
 );
